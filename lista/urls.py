2244bd67ac667d2edc756c79cd79a8aa65db5894
Gustavo Henrique <gustavo@gustavohenrique.net>
2012-05-02 17:41:58 -0300

    adicionado consulta ajax

--- lista/urls.py
+++ lista/urls.py
@@ -4,4 +4,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'lista.views.index', name='lista-index'),
     url(r'^resumo/$', 'lista.views.resumo', name='lista-resumo'),
+    url(r'^produtos/(?P<secao_id>[^/]+)', 'lista.views.produtos', name='lista-produtos-por-secao'),
+    #url(r'^produtos/(?P<secao_id>)\d/$', 'lista.views.produtos', name='lista-produtos-por-secao'),
 )
--- lista/views.py
+++ lista/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.views.generic.simple import direct_to_template
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponse, Http404
 from django.core.urlresolvers import reverse
+from django.core import serializers
 
-from lista.models import Secao
+
+from lista.models import Secao, Produto
 
 def index(request):
     context = {'secoes': Secao.objects.all()}
@@ -12,3 +14,12 @@ def index(request):
 
 def resumo(request):
     return direct_to_template(request, 'resumo.html')
+
+def produtos(request, secao_id):
+    secao = get_object_or_404(Secao, pk=secao_id)
+    produtos = Produto.objects.filter(secao=secao)
+    if produtos.count() > 0:
+        json = serializers.serialize('json', produtos, fields=('id','nome'))
+        return HttpResponse(json, mimetype='application/json')
+    else:
+        raise Http404
